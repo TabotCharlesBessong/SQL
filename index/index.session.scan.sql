fa1062a532e77684ab1b4271c29b4191e3d6fae8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-31 16:40:44 +0100

    key and non key index

--- index/index.session.scan.sql
+++ index/index.session.scan.sql
@@ -1,3 +1,9 @@
 -- explain analyze select id,g from grades where g > 80 and g < 95 order by g;
 
-SELECT * FROM grades;
+EXPLAIN ANALYZE SELECT g FROM grades WHERE id = 7;
+
+-- CREATE INDEX id_idx ON grades(id);
+
+-- DROP INDEX id_idx;
+
+-- CREATE INDEX id_idx ON grades(id) include (name);
